3549d16ffaf79c61936e0b64388ea5190a5b36a5
H2NCH2COOH <wzypublic@gmail.com>
2018-03-20 20:29:30 +0800

    Edit conntrack.h

--- conntrack.h
+++ conntrack.h
@@ -3,29 +3,42 @@
 
 #include <stdint.h>
 
-typedef struct
+typedef struct Conn4_s
 {
     uint8_t  int_addr[4];
     uint16_t int_port;
 
     uint8_t  ext_addr[4];
-    uint16_t nat_ext_port;
-    uint16_t ori_ext_port;
+    uint16_t ext_port;
+
+    uint16_t nat_port;
 
     uint32_t last_active;
+
+    struct Conn4_s* conn_ht_next;
+    struct Conn4_s* nat_ht_next;
+
+    struct Conn4_s* next;
+    struct Conn4_s* prev;
 } Conn4;
 
-typedef struct
+typedef struct Conn6_s
 {
     uint8_t  int_addr[16];
     uint16_t int_port;
 
     uint8_t  ext_addr[16];
-    uint16_t nat_ext_port;
-    uint16_t ori_ext_port;
+    uint16_t ext_port;
+
+    uint16_t nat_port;
 
     uint32_t last_active;
-} Conn6;
 
+    struct Conn6_s* conn_ht_next;
+    struct Conn6_s* nat_ht_next;
+
+    struct Conn6_s* next;
+    struct Conn6_s* prev;
+} Conn6;
 
 #endif /* _CONNTRACK_H_ */
