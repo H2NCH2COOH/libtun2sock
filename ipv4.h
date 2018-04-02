321d3b1a7471930d3108e6efd5b9223054d84e93
H2NCH2COOH <wzypublic@gmail.com>
2018-04-02 22:28:45 +0800

    Update ipv4.h

--- ipv4.h
+++ ipv4.h
@@ -28,12 +28,6 @@ typedef struct
         (hdr)->_b0 = ((hdr)->_b0 & 0x0F) | (((v) & 0x0F) << 4); \
     }while(0)
 
-#define ipv4_hdr_len(hdr)               (((hdr)->len[0] << 8) | (hdr)->len[1])
-#define ipv4_hdr_len_set(hdr, l)        do{ \
-        (hdr)->len[0] = ((l) & 0xFF00) >> 8; \
-        (hdr)->len[1] = (l) & 0xFF; \
-    }while(0)
-
 #define ipv4_hdr_ihl(hdr)               ((hdr)->_b0 & 0x0F)
 #define ipv4_hdr_ihl_set(hdr, v)        do{ \
         (hdr)->_b0 = ((hdr)->_b0 & 0xF0) | ((v) & 0x0F); \
@@ -49,11 +43,23 @@ typedef struct
         (hdr)->_b1 = ((hdr)->_b1 & 0xFC) | ((v) & 0x03); \
     }while(0)
 
+#define ipv4_hdr_len(hdr)               (((hdr)->len[0] << 8) | (hdr)->len[1])
+#define ipv4_hdr_len_set(hdr, l)        do{ \
+        (hdr)->len[0] = ((l) & 0xFF00) >> 8; \
+        (hdr)->len[1] = (l) & 0xFF; \
+    }while(0)
+
 #define ipv4_hdr_flags_DF               0x40
 #define ipv4_hdr_flags_MF               0x20
 #define ipv4_hdr_flags(hdr)             ((hdr)->_b6 & 0xE0)
 #define ipv4_hdr_flags_set(hdr, v)      do{ \
         (hdr)->_b6 = ((hdr)->_b6 & 0x1F) | ((v) & 0xE0); \
+    }while(0)
+
+#define ipv4_hdr_frag_offset(hdr)       ((((hdr)->_b6 & 0x1F) << 8) | (hdr)->_b7)
+#define ipv4_hdr_frag_offset_set(hdr, v)        do{ \
+        (hdr)->_b6 = ((hdr)->_b6 & 0xE0) | (((v) & 0x1F00) >> 8); \
+        (hdr)->_b7 = (v) & 0xFF; \
     }while(0)
 
 /***
