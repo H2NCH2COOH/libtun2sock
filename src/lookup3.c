700e5af1bfb02fbef460b2c3334ea1125b5feba6
Zhiyuan Wang <wzypublic@gmail.com>
2019-08-22 17:27:50 +0800

    Add fall through comments to lookup3.c to suppress warnings

--- src/lookup3.c
+++ src/lookup3.c
@@ -169,7 +169,9 @@ uint32_t        initval)         /* the previous hash, or an arbitrary value */
   switch(length)                     /* all the case statements fall through */
   {
   case 3 : c+=k[2];
+    /* fall-through */
   case 2 : b+=k[1];
+    /* fall-through */
   case 1 : a+=k[0];
     final(a,b,c);
   case 0:     /* case 0: nothing left to add */
@@ -215,7 +217,9 @@ uint32_t       *pb)               /* IN: more seed OUT: secondary hash value */
   switch(length)                     /* all the case statements fall through */
   {
   case 3 : c+=k[2];
+    /* fall-through */
   case 2 : b+=k[1];
+    /* fall-through */
   case 1 : a+=k[0];
     final(a,b,c);
   case 0:     /* case 0: nothing left to add */
